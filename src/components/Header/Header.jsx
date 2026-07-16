cf7dcc6bc0bbe68101f864d7ad6c64f1a930f796
Ruan Gomes <ruan.gmss@outlook.com>
2026-07-16 16:58:05 -0300

    feat: Estruturação do header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
+import dogs from '../../assets/dogs.svg';
+import user from '../../assets/user.svg';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="header-bg">
+      <nav className="header container">
+        <Link className="header-link" to="/">
+          <img src={dogs} alt="Logo Dogs" />
+        </Link>
+
+        <Link className="header-link" to="/login">
+          Login / Criar <img src={user} alt="Ícone de usuário" />
+        </Link>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
