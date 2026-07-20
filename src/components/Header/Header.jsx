b9b3122eb3d37e1936bf51ff175958e1612a617c
Ruan Gomes <ruan.gmss@outlook.com>
2026-07-20 12:56:01 -0300

    feat: Inclusão de lógica de logout no header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,10 +3,11 @@ import { Link } from 'react-router-dom';
 import './Header.css';
 import dogs from '../../assets/dogs.svg';
 import user from '../../assets/user.svg';
+import logout from '../../assets/logout.svg';
 import { UserContext } from '../../context/UserContext';
 
 const Header = () => {
-  const { data } = React.useContext(UserContext);
+  const { data, userLogout } = React.useContext(UserContext);
 
   return (
     <header className="header-bg">
@@ -15,15 +16,23 @@ const Header = () => {
           <img src={dogs} alt="Logo Dogs" />
         </Link>
 
-        {data ? (
-          <Link className="header-link" to="/perfil">
-            {data.nome} <img src={user} alt="Ícone de usuário" />
-          </Link>
-        ) : (
-          <Link className="header-link" to="/login">
-            Login / Criar <img src={user} alt="Ícone de usuário" />
-          </Link>
-        )}
+        <div className="header-links">
+          {data ? (
+            <Link className="header-link" to="/perfil">
+              {data.nome} <img src={user} alt="Ícone de usuário" />
+            </Link>
+          ) : (
+            <Link className="header-link" to="/login">
+              Login / Criar <img src={user} alt="Ícone de usuário" />
+            </Link>
+          )}
+
+          {data && (
+            <button>
+              <img src={logout} alt="Ícone de logout" onClick={userLogout} />
+            </button>
+          )}
+        </div>
       </nav>
     </header>
   );
