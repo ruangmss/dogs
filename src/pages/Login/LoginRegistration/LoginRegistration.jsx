9968090025e494d29f0f071105296920efc16df8
Ruan Gomes <ruan.gmss@outlook.com>
2026-07-20 13:21:22 -0300

    feat/refactor: Inclusão de hookk referente ao consumo de APIs e ajuste de cadastro de usuário utilizando o novo hook

--- src/pages/Login/LoginRegistration/LoginRegistration.jsx
+++ src/pages/Login/LoginRegistration/LoginRegistration.jsx
@@ -4,6 +4,8 @@ import Button from '../../../components/Form/Button/Button';
 import useForm from '../../../hooks/useForm';
 import { USER_POST } from '../../../api/api';
 import { UserContext } from '../../../context/UserContext';
+import useFetch from '../../../hooks/useFetch';
+import Error from '../../../components/Form/Error/Error';
 
 const LoginRegistration = () => {
   const username = useForm();
@@ -11,13 +13,14 @@ const LoginRegistration = () => {
   const password = useForm('password');
 
   const { userLogin } = React.useContext(UserContext);
+  const { error, loading, request } = useFetch();
 
   async function registrateUser(event) {
     event.preventDefault();
 
     const { url, options } = USER_POST(username.value, email.value, password.value);
 
-    const response = await fetch(url, options);
+    const { response } = await request(url, options);
 
     if (response.ok) {
       userLogin(username.value, password.value);
@@ -31,7 +34,10 @@ const LoginRegistration = () => {
         <Input label="Usuário" type="text" name="username" {...username} />
         <Input label="E-mail" type="email" name="email" {...email} />
         <Input label="Senha" type="password" name="password" {...password} />
-        <Button label="Cadastrar-se" />
+
+        {loading ? <Button disabled={true} label="Cadastrando..." /> : <Button label="Cadastrar-se" />}
+
+        <Error error={error} />
       </form>
     </section>
   );
