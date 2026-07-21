bf6212d290f585d6ae0a4e603756f745653fd6af
Ruan Gomes <ruan.gmss@outlook.com>
2026-07-21 17:53:39 -0300

    feat: Estruturação de componente de loading

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Feed from '../../components/Feed/Feed';
+import Loading from '../../components/Loading/Loading';
 
 const Home = () => {
   return (
     <section className="container main-container">
-      <Feed />
+      {/* <Feed /> */}
+      <Loading />
     </section>
   );
 };
