b72898eac9fdaeaee0378528d4cb7ecbf0fa38af
Ruan Gomes <ruan.gmss@outlook.com>
2026-07-22 16:02:56 -0300

    feat: Inclusão de estatísticas com lazy e suspense

--- src/components/UserStats/UserStats.jsx
+++ src/components/UserStats/UserStats.jsx
@@ -1,10 +1,51 @@
 import React from 'react';
 import useHead from '../../hooks/useHead';
+import './UserStats.css';
+import useFetch from '../../hooks/useFetch';
+import Loading from '../Loading/Loading';
+import Error from '../Form/Error/Error';
+import { STATS_GET } from '../../api/api';
+
+const UserStatsGraphs = React.lazy(() => import('./UserStatsGraphs/UserStatsGraphs'));
 
 const UserStats = () => {
   useHead('Estatísticas | Dogs', 'Confira as estatísticas das suas publicações no Dogs.');
 
-  return <div>UserStats</div>;
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    async function getData() {
+      const token = localStorage.getItem('token');
+
+      if (!token) {
+        return;
+      }
+
+      const { url, options } = STATS_GET(token);
+
+      await request(url, options);
+    }
+
+    getData();
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (error) {
+    return <Error error={error} />;
+  }
+
+  if (data) {
+    return (
+      <React.Suspense fallback={<Loading />} className="user-stats">
+        <UserStatsGraphs data={data} />
+      </React.Suspense>
+    );
+  }
+
+  return null;
 };
 
 export default UserStats;
